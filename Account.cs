f2e347ab5833d53a76ace7c1ef9e8912ad1c7212
yassmine <yassminemohamad2001@gmail.com>
2023-12-20 11:57:51 +0200

    first

---
+++ Account.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_day04_01
+{
+    internal class Account
+    {
+        private string _name;
+        
+        private decimal _balance;
+
+
+        //
+        public Account(string name, decimal startinBalance)
+        {
+            _name = name;
+            _balance = startinBalance;
+        }
+        //6
+        public bool Deposit(decimal amountToAdd)
+        {
+            if (amountToAdd > 0)
+            {
+                _balance += amountToAdd;
+                return true;
+            }
+            return false;
+        }
+        //7
+        public bool Withdraw( decimal amountToWithdarw) {
+            if (amountToWithdarw > 0 && amountToWithdarw <= _balance)
+            {
+                _balance -= amountToWithdarw;
+                return true;
+            }
+            return false;
+
+        }
+        //8
+        public string Name
+        {
+            get { return _name; }
+        }
+        //9
+        public void Print()
+        {
+            Console.WriteLine($"Name : {Name}");
+            Console.WriteLine($"Balance: {_balance}");
+        }
+
+
+
+        //Methods
+        //public void DisplayBalance();
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,98 @@
+﻿namespace task_day04_01
+{
+    enum MenuOptions
+    {
+        Withdraw=1, Deposit, Print, Quit
+
+    }
+
+
+    internal class Program
+    {
+        private static void ReadUserOptions(Account account)
+        {
+            MenuOptions option;
+            int intOption;
+            do
+            {
+                Console.WriteLine($"1-{MenuOptions.Withdraw}");
+                Console.WriteLine($"2-{MenuOptions.Deposit}");
+                Console.WriteLine($"3-{MenuOptions.Print}");
+                Console.WriteLine($"4-{MenuOptions.Quit}");
+
+                option = (MenuOptions)Convert.ToInt32(Console.ReadLine());
+                intOption = Convert.ToInt32(option);
+
+                switch (option)
+                {
+
+                    case MenuOptions.Deposit:
+                        {
+                            DoDeposit(account);
+                            break;
+                        }
+                    case MenuOptions.Withdraw:
+                        {
+                            DoWithdraw(account);
+                            break;
+                        }
+                    case MenuOptions.Print:
+                        {
+                            DoPrint(account);
+                            break;
+                        }
+                    case MenuOptions.Quit:
+                        { break; }
+                }
+            }
+
+            while (intOption >= 1 && intOption <= 4);
+        } 
+    
+        
+        private static void DoDeposit(Account account)
+        {
+            Console.WriteLine("Enter the Number to add:");
+            decimal amountToAdd=Convert.ToDecimal(Console.ReadLine());
+           if( account.Deposit(amountToAdd)) Console.WriteLine($"{amountToAdd} added successfully.");
+           else Console.WriteLine("unvalid amount ,please try again.");
+        }
+        private static void DoWithdraw(Account account) {
+            Console.WriteLine("Enter the Number to withdraw:");
+            decimal amountToWithdraw= Convert.ToDecimal(Console.ReadLine());
+          if(  account.Withdraw(amountToWithdraw)) Console.WriteLine($"{amountToWithdraw} withdrawn successfully.");
+            else Console.WriteLine("unvalid amount ,please try again.");
+            
+        }
+        private static void DoPrint(Account account)
+        { 
+            account.Print();
+        }
+
+        static void Main(string[] args)
+        {
+            //11
+            Account account = new Account("JAck", 20000);
+            account.Print();
+            account.Deposit(100);
+            account.Print();
+
+            //12
+            account.Withdraw(100);
+            account.Print();
+
+            //13
+
+
+            Account myAccount = new Account("yassmine", 100);
+            myAccount.Print();
+            myAccount.Deposit(400);
+            myAccount.Print();
+            myAccount.Withdraw(100);
+            myAccount.Print();
+
+            ReadUserOptions(myAccount); //400
+    }
+    }
+}
+
